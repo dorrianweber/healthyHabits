a987817806590b2735551ea6845ae7f2e6fe3c37
toasted-coder <christmanholmes21@gmail.com>
2021-05-25 16:13:50 -0700

    fixed quote api. also ran npm i outside of client folder so thats what those additions are

--- client/src/pages/Form.js
+++ client/src/pages/Form.js
@@ -4,70 +4,90 @@ import SleepingForm from "../components/SleepingForm";
 import SpendingForm from "../components/SpendingForm";
 
 function Form() {
+  const [showEating, setShowEating] = React.useState(false);
+  const [showSleeping, setShowSleeping] = React.useState(false);
+  const [showSpending, setShowSpending] = React.useState(false);
 
-    const [showEating, setShowEating] = React.useState(false)
-    const [showSleeping, setShowSleeping] = React.useState(false)
-    const [showSpending, setShowSpending] = React.useState(false)
+  const handleShowEating = () => {
+    setShowEating(true);
+    setShowSleeping(false);
+    setShowSpending(false);
+  };
 
-    const handleShowEating = () => {
-        setShowEating(true);
-        setShowSleeping(false);
-        setShowSpending(false);
-    };
+  const handleShowSleeping = () => {
+    setShowEating(false);
+    setShowSleeping(true);
+    setShowSpending(false);
+  };
 
-    const handleShowSleeping = () => {
-        setShowEating(false);
-        setShowSleeping(true);
-        setShowSpending(false);
-    };
+  const handleShowSpending = () => {
+    setShowEating(false);
+    setShowSleeping(false);
+    setShowSpending(true);
+  };
 
-    const handleShowSpending = () => {
-        setShowEating(false);
-        setShowSleeping(false);
-        setShowSpending(true);
-    };
+  const url = "https://api.quotable.io/random?tags=inspirational,famous-quotes";
 
-    return (
-        <>
-            <form className="form habit-form" onSubmit={(e) => {
-                e.preventDefault();
-                var selection = document.querySelector("#categories").value;
-                switch (selection) {
-                    case "eating":
-                        handleShowEating();
-                        break;
-                    case "sleeping":
-                        handleShowSleeping();
-                        break;
-                    case "spending":
-                        handleShowSpending();
-                        break;
-                    default:
-                        break;
-                };
+  function generateQuote() {
+    fetch(url)
+      .then(function (data) {
+        return data.json();
+      })
+      .then(function (data) {
+        document.getElementById("quote").innerHTML = data.content;
+        document.getElementById("author").innerHTML = "- " + data.author;
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+  // Repeat generateQuote() every 10 seconds
+  setInterval(generateQuote(), 10000);
+  //Note - 10000 milliseconds = 10
 
-            }}>
-                <label htmlFor="categories">Choose a category:</label>
-                <select name="categories" id="categories">
-                    <option value="eating">Eating</option>
-                    <option value="sleeping">Sleeping</option>
-                    <option value="spending">Spending</option>
-                </select>
+  return (
+    <>
+      <form
+        className="form habit-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          var selection = document.querySelector("#categories").value;
+          switch (selection) {
+            case "eating":
+              handleShowEating();
+              break;
+            case "sleeping":
+              handleShowSleeping();
+              break;
+            case "spending":
+              handleShowSpending();
+              break;
+            default:
+              break;
+          }
+        }}
+      >
+        <label htmlFor="categories">Choose a category:</label>
+        <select name="categories" id="categories">
+          <option value="eating">Eating</option>
+          <option value="sleeping">Sleeping</option>
+          <option value="spending">Spending</option>
+        </select>
 
-                <br />
+        <br />
 
-                <button className="btn btn-primary" id="category-next" type="submit">Next</button>
+        <button className="btn btn-primary" id="category-next" type="submit">
+          Next
+        </button>
+      </form>
 
-            </form>
-
-            <div id="formSpot">
-                {showEating && <EatingForm />}
-                {showSleeping && <SleepingForm />}
-                {showSpending && <SpendingForm />}
-            </div>
-        </>
-
-    );
-};
+      <div id="formSpot">
+        {showEating && <EatingForm />}
+        {showSleeping && <SleepingForm />}
+        {showSpending && <SpendingForm />}
+      </div>
+    </>
+  );
+}
 
 export default Form;
