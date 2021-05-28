b528c238a2432cc80f360340a558fc883de0d335
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-05-27 21:35:40 -0700

    Spendingform now works, pushes info to database. Fixed graph sizes, so that they don't change intermittently

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -7,36 +7,39 @@ function Profile() {
   let history = useHistory();
   const [state, dispatch] = useAuthState();
   if (!state.isAuthenticated) {
-    history.push("/login")
+    history.push("/login");
     return null;
   } else {
-        return (
-            <section class="profile">
-              <div class="user">
-                <img
-                  alt="HealthyHabbitsLogo"
-                  src="https://github.com/dorrianweber/project2/blob/main/public/images/healthyhabits.png?raw=true"
-                />
-                {/* <h2>{{user.name}} Profile</h2> */}
-                {/*Adding canvas graph area*/}
-                <div class="row3">
-                  <div class="graph">
-                    <p>Eating:</p>
-                        <EatGraph/>
-                  </div>
-                  <div class="graph">
-                    <p>Sleeping:</p>
-                        <SleepGraph/>
-                  </div>
-                  <div class="graph">
-                    <p>Spending:</p>
-                        <SpendGraph/>
-                  </div>
-                </div>
-              </div>
-            </section>
-          );
-    }
+    return (
+      <section className="container" style={{ paddingBottom: "70px" }}>
+        <img
+          alt="HealthyHabbitsLogo"
+          src="https://github.com/dorrianweber/project2/blob/main/public/images/healthyhabits.png?raw=true"
+        />
+        <div className="row">
+          <div className="col">
+            <div className="graph">
+              <p>Eating:</p>
+              <EatGraph />
+            </div>
+          </div>
+          <div className="col">
+            <div className="graph">
+              <p>Sleeping:</p>
+              <SleepGraph />
+            </div>
+          </div>
+          <div className="col">
+            <div className="graph">
+              <p>Spending:</p>
+              <SpendGraph />
+            </div>
+          </div>
+        </div>
+        <br />
+      </section>
+    );
   }
+}
 
 export default Profile;
