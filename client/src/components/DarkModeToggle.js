42d9660cff1e706c471f7209f1f5e86fe5322b8d
Matt H <matthewdherold@gmail.com>
2021-06-01 21:34:37 -0700

    added basic dark mode functionality

---
+++ client/src/components/DarkModeToggle.js
@@ -0,0 +1,18 @@
+import useDarkMode from "use-dark-mode";
+
+function DarkModeToggle() {
+const darkMode = useDarkMode(false);
+const darkSwitch = () => {
+    darkMode.toggle()
+    console.log(darkMode.value)
+}
+return (<div>☀
+    <label className="switch">
+  <input checked={darkMode.value} onChange={darkSwitch}  type="checkbox"/>
+  <span className="slider round"></span>
+</label>☾
+</div>
+)
+}
+
+export default DarkModeToggle
